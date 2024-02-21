3fdaaca8e070e19f18b274a85f63e36b97a5c6af
Jose Trista <jctristarosales@gmail.com>
2024-02-20 21:18:23 -0700

    Primer commit

---
+++ app/src/main/java/trista/josecarlos/popcornfactory/DetallePelicula.kt
@@ -0,0 +1,21 @@
+package trista.josecarlos.popcornfactory
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detalle_pelicula.*
+import kotlinx.android.synthetic.main.pelicula.iv_pelicula
+
+class DetallePelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_pelicula)
+
+        var bundle = intent.extras
+
+        if(bundle != null){
+            iv_pelicula_imagen.setImageResource(bundle.getInt("header"))
+            tv_nombre_pelicula.setText(bundle.getString("nombre"))
+            tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+        }
+    }
+}
---
+++ app/src/main/java/trista/josecarlos/popcornfactory/Pelicula.kt
@@ -0,0 +1,4 @@
+package trista.josecarlos.popcornfactory
+
+data class Pelicula ( var titulo: String, var image: Int, var header: Int, var sinopsis: String){
+}
